ea7848d8597f2196b3f8b5ed5a86d54d6d7b5aa9
John <bitancor@ualberta.ca>
2020-02-01 13:30:04 -0700

    modified routes to check for database with given login credentials

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ class Database:
         
         with self.connection.cursor() as cursor:
 
-            sql = "SELECT `username`, `password` FROM `users` WHERE `username`=%s AND `password`=%d"
+            sql = "SELECT `username`, `password` FROM `users` WHERE `username`=%s AND `password`=%s"
             cursor.execute(sql, (self.username, self.password, ))
             result = cursor.fetchone()
 
--- routes.py
+++ routes.py
@@ -1,29 +1,45 @@
+import os
+import jwt
+import datetime
 from aep import app
-from flask import jsonify
+from flask import jsonify, request
+from functools import wraps
+from database import Database
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') 
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return {'message': 'Token is missing or invalid'}
+
+        return f(*args, **kwargs)
+
+    return decorated
 
 @app.route("/")
 def main_():
-    return "<h1>Hello Bro!<h1>"
+    return "<h1>o!<h1>"
 
-@app.route("/sample", methods = ['GET', 'POST'])
-def api():
-    return jsonify(books)
-    
+@app.route("/protected")
+@token_required
+def protected():
+    return "Lmao gottem!"
+
+@app.route("/login", methods= ['GET'])
+def login():
+    headers = request.headers
+    username = headers.get("username")
+    password = headers.get("password")
+
+    login_credentials = Database(username, password)
+
+    if login_credentials:
+        token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return {"token": token.decode('UTF-8')}
+
+    return jsonify({"message": "ERROR: Unauthorized"}), 401
